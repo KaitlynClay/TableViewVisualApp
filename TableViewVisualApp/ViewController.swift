e372c8a80ac16186591b8e02024b246ffc24209f
Kaitlyn Clay <kclay@students.mchenry.edu>
2024-03-14 14:42:24 -0500

    Initial Commit

---
+++ TableViewVisualApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewVisualApp
+//
+//  Created by student on 3/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
